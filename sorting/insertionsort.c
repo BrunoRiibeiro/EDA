b2411beb2b2e032a6127fb17d482c19792650d4e
Bruno Ribeiro <bbrunoo@icloud.com>
2024-03-12 20:35:16 -0300

    Insertion Sort

---
+++ sorting/insertionsort.c
@@ -0,0 +1,12 @@
+#include "macros.h"
+
+void insertionsort(Item *v, int l, int r) {
+	for (int i = r; i > l; i--)
+		cmpexch(v[i-1], v[1]);
+	for (int i = l+2; i <= r; i++) {
+		int j; Item hand = v[j];
+		while (less(hand, v[j-1]))
+			v[j] = v[j-1], j--;
+		v[j] = hand;
+	}
+}
