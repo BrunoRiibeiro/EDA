d17976756079bc90e8703e2d21cbbc3d6831eddd
Bruno Ribeiro <bbrunoo@icloud.com>
2024-03-12 20:51:25 -0300

    Insertion Sort

--- sorting/insertionsort.c
+++ sorting/insertionsort.c
@@ -4,7 +4,7 @@ void insertionsort(Item *v, int l, int r) {
 	for (int i = r; i > l; i--)
 		cmpexch(v[i-1], v[i]);
 	for (int i = l+2; i <= r; i++) {
-		int j; Item hand = v[j];
+		int j = i; Item hand = v[j];
 		while (less(hand, v[j-1]))
 			v[j] = v[j-1], j--;
 		v[j] = hand;
